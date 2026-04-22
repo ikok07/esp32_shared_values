09a37d86cf8b947f445e4cd5935ac39c858cdfb1
Kaloyan Stefanov <kokmarok@gmail.com>
2026-04-22 21:30:22 +0300

    refactor to use event bits instead of subscriber queues

--- shared_values.c
+++ shared_values.c
@@ -9,13 +9,10 @@
 SHVAL_HandleTypeDef SHVAL_Init(SHVAL_ConfigTypeDef *Config) {
     SHVAL_HandleTypeDef handle = {
         .Value = Config->InitialValue,
+        .EventGroup = xEventGroupCreate(),
+        .SubscribersEventBits = Config->SubscribersEventBits,
         .Mutex = xSemaphoreCreateMutex()
     };
-
-    if (Config->SubscribersQueueSize > 0) {
-        handle.SubscribersQueue = xQueueCreate(Config->SubscribersQueueSize, sizeof(uint32_t));
-        handle.SubscribersCount = Config->SubscribersQueueSize;
-    }
 
     return handle;
 }
@@ -32,15 +29,11 @@ SHVAL_ErrorTypeDef SHVAL_GetValue(const SHVAL_HandleTypeDef *hshval, uint32_t *V
 
 SHVAL_ErrorTypeDef SHVAL_SetValue(SHVAL_HandleTypeDef *hshval, uint32_t Value, uint32_t TimeoutMS) {
     if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
+        xEventGroupClearBits(hshval->EventGroup, BIT0);
+
         hshval->Value = Value;
 
-        if (hshval->SubscribersQueue) {
-            // Queue is reset. All previous values are invalid
-            xQueueReset(hshval->SubscribersQueue);
-            for (int i = 0; i < hshval->SubscribersCount; i++) {
-                xQueueSend(hshval->SubscribersQueue, &Value, portMAX_DELAY);
-            }
-        }
+        xEventGroupSetBits(hshval->EventGroup, hshval->SubscribersEventBits);
 
         xSemaphoreGive(hshval->Mutex);
         return SHVAL_ERROR_OK;
@@ -49,17 +42,28 @@ SHVAL_ErrorTypeDef SHVAL_SetValue(SHVAL_HandleTypeDef *hshval, uint32_t Value, u
     }
 }
 
+SHVAL_ErrorTypeDef SHVAL_WaitForValue(SHVAL_HandleTypeDef *hshval, EventBits_t BitToWait, uint32_t *Value, TickType_t TicksToWait) {
+    SHVAL_ErrorTypeDef shval_err = SHVAL_ERROR_OK;
+    EventBits_t bits = xEventGroupWaitBits(hshval->EventGroup, BitToWait, pdTRUE, pdFALSE, TicksToWait);
+
+    if ((bits & BitToWait) != 0) {
+        if ((shval_err = SHVAL_GetValue(hshval, Value, pdTICKS_TO_MS(TicksToWait))) != SHVAL_ERROR_OK) {
+            return shval_err;
+        }
+        return SHVAL_ERROR_OK;
+    }
+
+    return SHVAL_ERROR_TIMEOUT;
+}
+
 SHVAL_PointerHandleTypeDef SHVAL_PointerInit(SHVAL_PointerConfigTypeDef *Config) {
     SHVAL_PointerHandleTypeDef handle = {
         .Value = Config->InitialValue,
         .ValueLen = Config->ValueLen,
+        .EventGroup = xEventGroupCreate(),
+        .SubscribersEventBits = Config->SubscribersEventBits,
         .Mutex = xSemaphoreCreateMutex()
     };
-
-    if (Config->SubscribersQueueSize > 0) {
-        handle.SubscribersQueue = xQueueCreate(Config->SubscribersQueueSize, sizeof(uint32_t));
-        handle.SubscribersCount = Config->SubscribersQueueSize;
-    }
 
     return handle;
 }
@@ -68,7 +72,7 @@ SHVAL_ErrorTypeDef SHVAL_PointerGetValue(const SHVAL_PointerHandleTypeDef *hshva
     uint32_t TimeoutMS) {
     if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
         memcpy(Value, hshval->Value, hshval->ValueLen);
-        *ValueLen = hshval->ValueLen;
+        if (ValueLen != NULL) *ValueLen = hshval->ValueLen;
         xSemaphoreGive(hshval->Mutex);
         return SHVAL_ERROR_OK;
     } else {
@@ -81,17 +85,25 @@ SHVAL_ErrorTypeDef SHVAL_PointerSetValue(SHVAL_PointerHandleTypeDef *hshval, voi
     if (xSemaphoreTake(hshval->Mutex, pdMS_TO_TICKS(TimeoutMS))) {
         memcpy(hshval->Value, Value, hshval->ValueLen);
 
-        if (hshval->SubscribersQueue) {
-            // Queue is reset. All previous values are invalid
-            xQueueReset(hshval->SubscribersQueue);
-            for (int i = 0; i < hshval->SubscribersCount; i++) {
-                xQueueSend(hshval->SubscribersQueue, &Value, portMAX_DELAY);
-            }
-        }
+        xEventGroupSetBits(hshval->EventGroup, hshval->SubscribersEventBits);
 
         xSemaphoreGive(hshval->Mutex);
         return SHVAL_ERROR_OK;
     } else {
         return SHVAL_ERROR_VAL_UNAVAILABLE;
     }
+}
+
+SHVAL_ErrorTypeDef SHVAL_PointerWaitForValue(SHVAL_PointerHandleTypeDef *hshval, EventBits_t BitToWait, void *Value, uint32_t *ValueLen, TickType_t TicksToWait) {
+    SHVAL_ErrorTypeDef shval_err = SHVAL_ERROR_OK;
+    EventBits_t bits = xEventGroupWaitBits(hshval->EventGroup, BitToWait, pdTRUE, pdFALSE, TicksToWait);
+
+    if ((bits & BitToWait) != 0) {
+        if ((shval_err = SHVAL_PointerGetValue(hshval, Value, ValueLen, pdTICKS_TO_MS(TicksToWait))) != SHVAL_ERROR_OK) {
+            return shval_err;
+        }
+        return SHVAL_ERROR_OK;
+    }
+
+    return SHVAL_ERROR_TIMEOUT;
 }
